251b7d83d6a580c28a33257df2e4dfc0387d51dc
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-06-11 14:14:45 -0700

    added button functionality so that when the user clicks on any button, the G API outputs only 5 results on less

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,6 +1,15 @@
 var longitude
 
 var latitude
+var value;
+
+$(".gallery").on("click", function () {
+  value = $(this).children().attr("data-tattoo-style");
+  alert(value);
+});
+
+
+
 
 ///GEO LOCATION API
 var x = document.getElementById("demo");
@@ -18,10 +27,8 @@ getLocation();
 function showPosition(position) {
   console.log(position);
   longitude = position.coords.longitude;
-  
+
   latitude = position.coords.latitude;
-
-
 }
 
 function displayTatooShopResult(shopData) {
@@ -38,12 +45,12 @@ function displayTatooShopResult(shopData) {
 
 
 // this function records the changes in the tattoo-style-selection
-$("#black-and-gray").on("click", function () {
+$(".gallery").on("click", function () {
 
   console.log(latitude);
   console.log(longitude);
- 
-  var queryUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=black+gray+tatoos&location=" + latitude + "," + longitude + "&radius=20000&key=AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
+
+  var queryUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + value + "&location=" + latitude + "," + longitude + "&radius=20000&key=AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
   var apiKey = "AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
   console.log(queryUrl)
   // Performing our AJAX GET request
@@ -56,71 +63,10 @@ $("#black-and-gray").on("click", function () {
     saveResponse = response;
     $("#populator").empty();
     // link the API results here
-    $("#populator").append("<p>" + saveResponse.results[0].name + " | " + "Rating: " + saveResponse.results[0].rating + " | " + saveResponse.results[0].formatted_address + " | " + saveResponse.results[0].photos[0].html_attributions[0]);
-    displayTatooShopResult(response.results[0]);
-    displayTatooShopResult(response.results[1]);
-    displayTatooShopResult(response.results[10]);
-    displayTatooShopResult(response.results[2]);
+
+    for (var i = 0; i < Math.min(response.results.length, 5); i++) {
+      displayTatooShopResult(response.results[i]);
+      console.log(response.results[i])
+    }
   });
 });
-
-$("#new-school").on("click", function () {
-
-  var queryUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=new+school+tatoos+in+San+Diego&key=AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
-  var apiKey = "AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
-  // Performing our AJAX GET request
-  var saveResponse;
-  $.ajax({
-    url: queryUrl,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
-    saveResponse = response;
-    $("#populator").empty();
-    $("#populator").append("<p>" + saveResponse.results[0].name + " | " + "Rating: " + saveResponse.results[0].rating + " | " + saveResponse.results[0].formatted_address + " | " + saveResponse.results[0].photos[0].html_attributions[0]);
-    displayTatooShopResult(response.results[0]);
-    displayTatooShopResult(response.results[1]);
-    displayTatooShopResult(response.results[10]);
-    displayTatooShopResult(response.results[2]);
-  });
-});
-
-$("#old-school").on("click", function () {
-  var queryUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=black+gray+tatoos+in+San+Diego&key=AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
-  var apiKey = "AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
-  // Performing our AJAX GET request
-  var saveResponse;
-  $.ajax({
-    url: queryUrl,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
-    saveResponse = response;
-    // link the API results here
-    $("#populator").empty();
-
-  });
-
-  $("#japanese").on("click", function () {
-    var queryUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=black+gray+tatoos+in+San+Diego&key=AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
-    var apiKey = "AIzaSyDYw0kEgd0PxKKZZ5GGBBgeygLHxpwv9bA";
-    // Performing our AJAX GET request
-    var saveResponse;
-    $.ajax({
-      url: queryUrl,
-      method: "GET"
-    }).then(function (response) {
-      console.log(response);
-      saveResponse = response;
-      // alert($(this).val());
-      // link the API results here
-      // $("#populator").empty();
-      $("#populator").append("<p>" + saveResponse.results[0].name + " | " + "Rating: " + saveResponse.results[0].rating + " | " + saveResponse.results[0].formatted_address + " | " + saveResponse.results[0].photos[0].html_attributions[0]);
-      displayTatooShopResult(response.results[0]);
-      displayTatooShopResult(response.results[1]);
-      displayTatooShopResult(response.results[10]);
-      displayTatooShopResult(response.results[2]);
-    });
-  });
-});
-
